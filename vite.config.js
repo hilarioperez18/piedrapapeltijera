e804363945e06b2cfa7008c9375889babd51bf39
Hilario <hilarioperez18@gmail.com>
2023-10-03 21:11:15 +0200

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import path from "node:path";
 
 const isGitHubPages = true;
-const folderName = path.basename(process.cwd()) + "/";
+const folderName = `${path.basename(process.cwd())}/`;
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+const base = mode === "production" && isGitHubPages ? `/${folderName}` : "/";
 
 export default defineConfig({
   root: "src",
